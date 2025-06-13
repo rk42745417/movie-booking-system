e26d3eab3572c20c179934cc9c99d8085dee6708
AL227git <ang227git@gmail.com>
2025-06-13 20:24:56 +0800

    Add javadoc to TicketType.java

--- movie-booking-app/src/main/java/com/javaoop/movie_booking_app/model/TicketType.java
+++ movie-booking-app/src/main/java/com/javaoop/movie_booking_app/model/TicketType.java
@@ -1,6 +1,16 @@
 package com.javaoop.movie_booking_app.model;
 
+/**
+ * Enum representing the types of tickets available for booking.
+ */
 public enum TicketType {
+    /**
+     * Ticket type for adult customers.
+     */
     ADULT,
+
+    /**
+     * Ticket type for children customers.
+     */
     CHILDREN
 }
