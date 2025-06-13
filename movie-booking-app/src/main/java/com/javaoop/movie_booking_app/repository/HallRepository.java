229644f8508d4b9708da28a389cbb0c19b1c0dc4
AL227git <ang227git@gmail.com>
2025-06-13 20:25:32 +0800

    Add javadoc to HallRepository.java

--- movie-booking-app/src/main/java/com/javaoop/movie_booking_app/repository/HallRepository.java
+++ movie-booking-app/src/main/java/com/javaoop/movie_booking_app/repository/HallRepository.java
@@ -4,10 +4,12 @@ import com.javaoop.movie_booking_app.model.Hall;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+/**
+ * Repository interface for managing {@link Hall} entities.
+ * 
+ * Extends {@link JpaRepository} to provide CRUD operations and pagination
+ * capabilities for Hall objects.
+ */
 @Repository
 public interface HallRepository extends JpaRepository<Hall, Long> {
 }
-
-
-
