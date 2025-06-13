0f5528b885822ee322a2d291862b837c8ec90193
AL227git <ang227git@gmail.com>
2025-06-13 20:19:48 +0800

    Add javadoc to HomeController.java

--- movie-booking-app/src/main/java/com/javaoop/movie_booking_app/controller/HomeController.java
+++ movie-booking-app/src/main/java/com/javaoop/movie_booking_app/controller/HomeController.java
@@ -2,10 +2,26 @@ package com.javaoop.movie_booking_app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * HomeController is responsible for handling the root URL ("/") of the application.
+ * It directs users to the homepage.
+ */
 @Controller
 public class HomeController {
+
+    /**
+     * Default no-argument constructor.
+     */
+    public HomeController() {
+        // no-op
+    }
+    
+    /**
+     * Handles requests to the root URL ("/").
+     *
+     * @return the name of the Thymeleaf view to be rendered ("index")
+     */
     @GetMapping()
     public String home() {
         return "index";
